c4425d9b4d8a4adf378379bbc6f796055b6dc50d
abbasy001 <younes@abbas.cc>
2014-03-19 12:46:36 +0100

    2 naar 1

--- CommercialQuiz/CommercialQuiz/CommercialQuiz/MainActivity.cs
+++ CommercialQuiz/CommercialQuiz/CommercialQuiz/MainActivity.cs
@@ -11,7 +11,7 @@ namespace CommercialQuiz
 	[Activity (Label = "CommercialQuiz", MainLauncher = true)]
 	public class MainActivity : Activity
 	{
-		int count = 2;
+		int count = 1;
 
 		protected override void OnCreate (Bundle bundle)
 		{
